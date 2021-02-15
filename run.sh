d42b3092f04cd3c16cb9d1235f55f53b621ddc31
Junoh Moon <mjo970625@gmail.com>
2021-02-15 14:25:53 +0900

    Add configurations with different bad block ratio

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-folder=results/miniature
-parallel --sshdelay 0.1 --sshlogin n0006,n0010 --shuf --eta \
+folder=results/threeDifferentCases
+parallel --sshdelay 0.1 --sshlogin 8/n0011,8/n0012,8/n0014,8/n0015 --shuf --eta \
 	cd simplessd-standalone \&\& time ./simplessd-standalone {1} {2} "$folder"/{1/.}/{2/.} \
-	::: config/msnfs_sampled.cfg config/msnfs_writeonly_sampled.cfg config/exchange_sampled.cfg config/exchange_writeonly_sampled.cfg config/ycsb_sampled.cfg config/ycsb_writeonly_sampled.cfg config/randwrite.cfg \
-	::: simplessd/config/small.cfg simplessd/config/small_optimized.cfg
+	::: config/msnfs_sampled.cfg config/msnfs_writeonly_sampled.cfg config/exchange_sampled.cfg config/exchange_writeonly_sampled.cfg config/ycsb_sampled.cfg config/ycsb_writeonly_sampled.cfg \
+	::: simplessd/config/small.cfg simplessd/config/small_optimized.cfg simplessd/config/worse.cfg simplessd/config/worse_optimized.cfg simplessd/config/worst.cfg simplessd/config/worst_optimized.cfg
 	#::: config/msnfs.cfg config/msnfs_writeonly.cfg config/exchange.cfg config/exchange_writeonly.cfg config/ycsb.cfg config/ycsb_writeonly.cfg config/randwrite.cfg \
 	#::: simplessd/config/mine.cfg simplessd/config/mine_optimized.cfg
