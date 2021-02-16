0bdee9b55401a9706e1dddada3a2f648de12da53
Junoh Moon <mjo970625@gmail.com>
2021-02-16 09:51:33 +0900

    Trace progress on tmux panes

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 folder=results/threeDifferentCases
-parallel --sshdelay 0.1 --sshlogin 8/n0011,8/n0012,8/n0014,8/n0015 --shuf --eta \
+parallel --sshdelay 0.1 --sshlogin 8/n0011,8/n0012,8/n0014,8/n0015 --shuf --eta --tmuxpane \
 	cd simplessd-standalone \&\& time ./simplessd-standalone {1} {2} "$folder"/{1/.}/{2/.} \
 	::: config/msnfs_sampled.cfg config/msnfs_writeonly_sampled.cfg config/exchange_sampled.cfg config/exchange_writeonly_sampled.cfg config/ycsb_sampled.cfg config/ycsb_writeonly_sampled.cfg \
 	::: simplessd/config/small.cfg simplessd/config/small_optimized.cfg simplessd/config/worse.cfg simplessd/config/worse_optimized.cfg simplessd/config/worst.cfg simplessd/config/worst_optimized.cfg
