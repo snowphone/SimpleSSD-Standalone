a0a64b08cf928b8217e3541188cdb8dfeee6e9a4
Junoh Moon <mjo970625@gmail.com>
2021-05-13 00:25:14 +0900

    Apply lognormal model with two sigma values

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
-folder=results/withoutHotcold
+folder=results/lognormal
 parallel --eta --tmuxpane \
 	time ./simplessd-standalone {1} {2} "$folder"/{1/.}/{2/.} \
 	::: config/alibaba_sampled.cfg \
 		config/exchange_sampled.cfg \
 		config/msnfs_sampled.cfg \
 		config/systor17_sampled.cfg \
-	::: simplessd/config/1e-12.cfg simplessd/config/1e-12_optimized.cfg \
-		simplessd/config/5e-12.cfg simplessd/config/5e-12_optimized.cfg \
-		simplessd/config/1e-11.cfg simplessd/config/1e-11_optimized.cfg \
-		simplessd/config/5e-11.cfg simplessd/config/5e-11_optimized.cfg \
-		simplessd/config/1e-10.cfg simplessd/config/1e-10_optimized.cfg \
-		simplessd/config/3.32e-11.cfg simplessd/config/3.32e-11_optimized.cfg
+	::: simplessd/config/1e-12.cfg simplessd/config/1e-12_optimized.cfg simplessd/config/1e-12_optimized_harsh.cfg \
+		simplessd/config/5e-12.cfg simplessd/config/5e-12_optimized.cfg simplessd/config/5e-12_optimized_harsh.cfg \
+		simplessd/config/1e-11.cfg simplessd/config/1e-11_optimized.cfg simplessd/config/1e-11_optimized_harsh.cfg \
+		simplessd/config/5e-11.cfg simplessd/config/5e-11_optimized.cfg simplessd/config/5e-11_optimized_harsh.cfg \
+		simplessd/config/1e-10.cfg simplessd/config/1e-10_optimized.cfg simplessd/config/1e-10_optimized_harsh.cfg \
+		simplessd/config/3.32e-11.cfg simplessd/config/3.32e-11_optimized.cfg simplessd/config/3.32e-11_optimized_harsh.cfg 
 
